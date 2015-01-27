eb2db69217c505c5a723efceb2ab69171917c8c6
Nicola Giuliani <ngiuliani@sissa.it>
2015-01-27 16:47:10 +0100

    solved something

--- include/exceptions.h
+++ include/exceptions.h
@@ -4,17 +4,17 @@
 *    @author  MHPC
 *    @date    2015.1.~27
 */ 
-
+#include <exception>
 /**
 * This class is the base class for all exception classes. Do not use
 * its methods and variables directly since the interface and
 * mechanism may be subject to change.
 */
-#define DeclException0(Exception0)  class Exception0 :  public ExceptionBase {}
+#define DeclException0(Exception0)  class Exception0 :  public std::exception {}
 
 
 #define DeclException3(Exception3, type1, type2, type3, outsequence)      \
-  class Exception3 : public ExceptionBase {                       \
+  class Exception3 : public std::exception {				\
   public:                                                                 \
     Exception3 (const type1 a1, const type2 a2, const type3 a3) :         \
       arg1 (a1), arg2(a2), arg3(a3) {}                                    \
@@ -35,6 +35,8 @@
 * introduce this special assertion for ExcIndexRange in order
 * to keep the user codes shorter.
 */
+
+#define Assert(cond, exc) {}
 
 #define AssertIndexRange(index,range) Assert((index) < (range), ExcIndexRange((index),0,(range))) {}
 
