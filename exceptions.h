2d9dd2988ec495524b15df2064647e0a21d3fdf9
Mauro <esenonfossiio@gmail.com>
2015-01-27 12:26:45 +0100

    HEADER for exceptions

---
+++ exceptions.h
@@ -0,0 +1,12 @@
+/*!
+*    @file    MyHeaderFile.h
+*    @brief   Brief header file description
+*    @author  MHPC
+*    @date    2015.1.~27
+*/ 
+
+/**
+* This class is the base class for all exception classes. Do not use
+* its methods and variables directly since the interface and
+* mechanism may be subject to change.
+*/
